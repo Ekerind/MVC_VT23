a755bbc3f5544bde06cbb5c55860befb0260d5f1
Sora <maek22@student.bth.se>
2023-04-18 17:48:24 +0200

    Added Card Game landing page

---
+++ src/Controller/CardGameController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CardGameController extends AbstractController
+{
+    #[Route("/card", name: "card_index")]
+    public function home(): Response
+    {
+        return $this->render('cards/index.html.twig');
+    }
+
+}
